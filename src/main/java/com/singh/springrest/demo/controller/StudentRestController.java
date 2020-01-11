6dc4c2c0bd22aa7658d929c07c3d4d437fd5e619
Amrit Singh <asingh1092@gmail.com>
2020-01-11 13:01:00 -0500

    refactor code for @PostConstruct

--- src/main/java/com/singh/springrest/demo/controller/StudentRestController.java
+++ src/main/java/com/singh/springrest/demo/controller/StudentRestController.java
@@ -3,6 +3,8 @@ package com.singh.springrest.demo.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,26 +16,27 @@ import com.singh.springrest.demo.model.Student;
 @RequestMapping("/api")
 public class StudentRestController {
 	
-	@GetMapping("/students")
-	public List<Student> getStudents() {
-		List<Student> students = new ArrayList<Student>();
-		
-		students.add(new Student("Amrit", "Singh"));
-		students.add(new Student("John", "Doe"));
-		students.add(new Student("Mary", "Sue"));
-		
-		return students;
-	}
+	private List<Student> students;
 	
-	@GetMapping("/student/{studentId}")
-	public Student getStudentsId(@PathVariable int studentId) {
-		List<Student> students = new ArrayList<Student>();
-		
+	// define @PostConstruct data to load the student data
+	@PostConstruct
+	public void loadData() {
+		students = new ArrayList<>();
 		students.add(new Student(0, "Mario", "Rossi"));
 		students.add(new Student(1, "Amrit", "Singh"));
 		students.add(new Student(2, "John", "Doe"));
 		students.add(new Student(3, "Mary", "Sue"));
-		
+	}
+	
+	// define endpoint for "/students" - returns list of students
+	@GetMapping("/students")
+	public List<Student> getStudents() {
+		return students;
+	}
+	
+	//define endpoint for "/student/{studentId}" - returns student 
+	@GetMapping("/student/{studentId}")
+	public Student getStudentsId(@PathVariable int studentId) {
 		return students.get(studentId);
 	}
 
